ef403b089b834cd88711d34bb47d1fa15b6c4625
nadellatx <nadellas@live.com>
2023-03-05 02:26:10 +0000

    after Xray subsegment changes

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -124,6 +124,7 @@ def data_create_message():
   return
 
 @app.route("/api/activities/home", methods=['GET'])
+@xray_recorder.capture('activities_home')
 def data_home():
   data = HomeActivities.run()
   return data, 200
@@ -165,6 +166,7 @@ def data_activities():
   return
 
 @app.route("/api/activities/<string:activity_uuid>", methods=['GET'])
+@xray_recorder.capture('activities_show')
 def data_show_activity(activity_uuid):
   data = ShowActivity.run(activity_uuid=activity_uuid)
   return data, 200
